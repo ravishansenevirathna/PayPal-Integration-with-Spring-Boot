ac995a2833891ad8c0e36e6628a809c1c43f8b74
ravishansenevirathna <travishan55@gmail.com>
2024-05-25 21:50:07 +0530

    first commit

---
+++ src/main/java/com/shopping/payment/config/PaypalConfig.java
@@ -0,0 +1,31 @@
+package com.shopping.payment.config;
+
+import com.paypal.base.rest.APIContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Hi 👋, I'm ravishansenevirathna
+ * Project : paypal-integration
+ * Created date : 5/25/2024
+ * Created time : 9:38 PM
+ */
+
+@Configuration
+public class PaypalConfig {
+
+    @Value("${paypal.client-id}")
+    private String clientId;
+
+    @Value("${paypal.client-secret}")
+    private String clientSecret;
+
+    @Value("${paypal.mode}")
+    private String mode;
+
+    @Bean
+    public APIContext apiContext() {
+        return new APIContext(clientId, clientSecret, mode);
+    }
+}
